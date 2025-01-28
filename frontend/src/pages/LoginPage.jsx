520287885ed9903f54fc1410c9ee5b2f86a9ee19
slowmfy <aminecheikh17@gmail.com>
2025-01-28 21:33:51 +0100

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -26,7 +26,7 @@ export const LoginPage = () => {
 
     if (Object.keys(validationErrors).length === 0) {
       try {
-        const response = await fetch("http://127.0.0.1:8000/api/token/", {
+        const response = await fetch("http://127.0.0.1:8000/apif/token/", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
